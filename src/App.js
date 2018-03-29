a7506ed71f215d46659d1f98df547032a7fb7c7b
Samuel Gray <sam.gray22@gmail.com>
2018-03-29 17:04:32 +0100

    React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <p>hello</p>
+        <h1>YO!</h1>
+      </div>
+    );
+  }
+};
+
+export default App;
